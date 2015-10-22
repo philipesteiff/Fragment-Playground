b53b9817cf8594feee78c7b3137dae607c5d7876
Philipe Steiff <philipe.loyola@concrete.com.br>
2015-10-22 11:46:30 -0200

    Criando playground de fragment

--- app/src/main/java/com/example/fragmentsexample/DummyFragment.java
+++ app/src/main/java/com/example/fragmentsexample/DummyFragment.java
@@ -10,13 +10,16 @@ import android.view.ViewGroup;
 /**
  * Created by philipesteiff on 10/9/15.
  */
-public class TestFragment extends Fragment {
+public class DummyFragment extends Fragment {
 
-    public static final String TAG = "TestFragment";
+    public static final String TAG = "DummyFragment";
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.test_fragment, container, false);
+        return inflater.inflate(R.layout.dummy_fragment, container, false);
     }
 }
--- app/src/main/java/com/example/fragmentsexample/MainActivity.java
+++ app/src/main/java/com/example/fragmentsexample/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.fragmentsexample;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -18,14 +15,14 @@ import butterknife.OnClick;
  * ~ O que são fragments?
  * Fragments representam uma porção de tela, modularizando sua tela em pequenas partes. Um Fragment deve sempre
  * ser incorporado a uma Activity e seu lifecycle é diretamente afetada pela Activity que o hospeda.
- *
- *
+ * <p/>
+ * <p/>
  * ~ Vantagens
  * + Reutilização:
- *
  */
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +33,57 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Ativa o log de transações do fragmentManager
+        FragmentManager.enableDebugLogging(true);
     }
 
-    @OnClick(R.id.one)
-    public void onClick(View view) {
-        startActivity(new Intent(this, OptionOneActivity.class));
+    @OnClick(R.id.attach)
+    public void onAttachClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.viewgroup_fragment_container);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.attach(fragment);
+        fragmentTransaction.commit();
     }
+
+
+    @OnClick(R.id.add)
+    public void onAddClick(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.viewgroup_fragment_container, new DummyFragment(), DummyFragment.TAG);
+        fragmentTransaction.commit();
+    }
+
+
+    @OnClick(R.id.remove)
+    public void onRemoveClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.viewgroup_fragment_container);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.remove(fragment);
+        fragmentTransaction.commit();
+    }
+
+g
+    @OnClick(R.id.detach)
+    public void onDetachClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.viewgroup_fragment_container);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.detach(fragment);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.pop_back_stack)
+    public void onPopBackStackClick(View view) {
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @OnClick(R.id.debug)
+    public void onDebugClick() {
+
+    }
+
+
 
 }
