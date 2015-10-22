82da81cc841cd6129a8ee476b6f649b26735a074
Philipe Steiff <philipe.loyola@concrete.com.br>
2015-10-22 17:30:56 -0200

    Finalizando playground

--- app/src/main/java/com/example/fragmentsexample/DummyFragment.java
+++ app/src/main/java/com/example/fragmentsexample/DummyFragment.java
@@ -1,11 +1,20 @@
 package com.example.fragmentsexample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by philipesteiff on 10/9/15.
@@ -13,13 +22,68 @@ import android.view.ViewGroup;
 public class DummyFragment extends Fragment {
 
     public static final String TAG = "DummyFragment";
+    public static final String ARG_TITLE = "ARG_TITLE";
 
+    @Bind(R.id.text_test)
+    TextView textViewTest;
 
+    @Bind(R.id.edit_test)
+    EditText editTextTest;
 
+    // - Padrão comum para inicializar fragments com argumentos
+    public static DummyFragment newInstance(final String title) {
+        DummyFragment fragment = new DummyFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
+    // - Primeiro metodo chamado, antes da criacao de qualquer view.
+    // - Chamado quando a instancia do fragment é associado a activity.
+    // - Não é garantido que a activity foi completamente inicializada.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    // - Segundo método chamado, antes das views do fragment serem criadas.
+    // - O onCreate é chamado quando a instancia do Fragment é criada ou recriada.
+    // - O método onCreate é chamado quando a instância do Fragment está sendo criado, ou recriado.
+    // - Não é garantido que a activity foi completamente inicializada.
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // - É chamado para o fragment criar sua propria view.
+    // - Retornando null o fragment passa a não ter view.
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dummy_fragment, container, false);
+        View view = inflater.inflate(R.layout.dummy_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
+
+    // - Normalmente utilizado para setar valores e listeners as views.
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textViewTest.setText(getArguments().getString(ARG_TITLE));
+    }
+
+    // - Chamado depois que a activity host completar o onCreate().
+    // - Qualquer acesso a hieraquia de view da activity host deve ser feita aqui.
+    // - Nesse ponto é seguro interagir com a activity.
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @OnClick(R.id.button_change_test)
+    public void OnChangeTextClick(View view) {
+        editTextTest.setText(UUID.randomUUID().toString());
+    }
+
 }
---
+++ app/src/main/java/com/example/fragmentsexample/FragmentPlaygroundActivity.java
@@ -0,0 +1,135 @@
+package com.example.fragmentsexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * ~ O que são fragments?
+ * Fragments representam uma porção de tela, modularizando sua tela em pequenas partes. Um Fragment deve sempre
+ * ser incorporado a uma Activity e seu lifecycle é diretamente afetada pela Activity que o hospeda.
+ * <p>
+ * <p>
+ * ~ Vantagens
+ * + Reutilização:
+ */
+
+public class FragmentPlaygroundActivity extends AppCompatActivity {
+
+    private boolean enableAddToBackStack;
+    private boolean enableRetainInstance;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.toogle_button_back_stack)
+    ToggleButton toggleButtonBackStack;
+
+    @Bind(R.id.toogle_button_retain_instance)
+    ToggleButton toggleButtonRetainInstance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        toggleButtonBackStack.setOnCheckedChangeListener(onAddToBackStackCheckedChangeListener);
+        toggleButtonRetainInstance.setOnCheckedChangeListener(onRetainInstanceCheckedChangeListener);
+
+        // Ativa o log de transações do fragmentManager
+        FragmentManager.enableDebugLogging(true);
+    }
+
+    @OnClick(R.id.add)
+    public void onAddClick(View view) {
+        Fragment fragment = DummyFragment.newInstance("Meu Fragment");
+        if(enableRetainInstance)
+            fragment.setRetainInstance(true);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.viewgroup_fragment_container, fragment, DummyFragment.TAG);
+        if (enableAddToBackStack)
+            fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.attach)
+    public void onAttachClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(DummyFragment.TAG);
+        if(enableRetainInstance)
+            fragment.setRetainInstance(true);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.attach(fragment);
+        if (enableAddToBackStack)
+            fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.remove)
+    public void onRemoveClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(DummyFragment.TAG);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.remove(fragment);
+        if (enableAddToBackStack)
+            fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
+    @OnClick(R.id.detach)
+    public void onDetachClick(View view) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.viewgroup_fragment_container);
+        if(enableRetainInstance)
+            fragment.setRetainInstance(true);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.detach(fragment);
+        if (enableAddToBackStack)
+            fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.replace)
+    public void onReplaceClick(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.viewgroup_fragment_container, DummyFragment.newInstance("Novo Fragment"), DummyFragment.TAG);
+        if (enableAddToBackStack)
+            fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick(R.id.pop_back_stack)
+    public void onPopBackStackClick(View view) {
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @OnClick(R.id.debug)
+    public void onDebugClick() {
+
+    }
+
+    CompoundButton.OnCheckedChangeListener onAddToBackStackCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            enableAddToBackStack = isChecked;
+        }
+    };
+
+    CompoundButton.OnCheckedChangeListener onRetainInstanceCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            enableRetainInstance = isChecked;
+        }
+    };
+
+}
